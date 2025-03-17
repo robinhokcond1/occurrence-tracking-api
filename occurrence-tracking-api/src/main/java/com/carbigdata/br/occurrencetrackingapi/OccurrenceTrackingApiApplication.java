666552c25e3829dd7ccd685efefcb3319ee1887b
Robinhokcond1 <robinhokcond1@hotmail.com>
2025-03-17 20:10:40 -0300

    configuracoes iniciais do projeto.

---
+++ occurrence-tracking-api/src/main/java/com/carbigdata/br/occurrencetrackingapi/OccurrenceTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.carbigdata.br.occurrencetrackingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccurrenceTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccurrenceTrackingApiApplication.class, args);
+	}
+
+}
---
+++ occurrence-tracking-api/src/test/java/com/carbigdata/br/occurrencetrackingapi/OccurrenceTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbigdata.br.occurrencetrackingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccurrenceTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
